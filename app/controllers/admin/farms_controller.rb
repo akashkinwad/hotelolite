fd2d6c45f45170f3698183e6d803c3d65471d09b
Akash Kinwad <akashkinwad@gmail.com>
2020-03-05 00:33:23 +0530

    farm active, is_featured function from admin

---
+++ app/controllers/admin/farms_controller.rb
@@ -0,0 +1,38 @@
+class Admin::FarmsController < Admin::AdminsController
+  before_action :set_farm, only: [:edit, :update, :destroy, :show]
+
+  def index
+    @farms = Farm.all.page(params[:page]).per(20)
+  end
+
+  def show;end
+
+  def edit;end
+
+  def update
+    if @farm.update(farm_params)
+      redirect_to admin_farms_path, notice: 'Farm was added successfully.'
+    else
+      redirect_to admin_farms_path, error: 'Error in adding farm'
+    end
+  end
+
+  def destroy
+    @farm.destroy
+    redirect_to admin_farms_path, notice: 'Farm was deleted successfully.'
+  end
+
+  private
+
+  def set_farm
+    @farm = Farm.find(params[:id])
+  end
+
+  def farm_params
+    params.require(:farm).permit(:status, :is_featured)
+  end
+
+  def set_breadcrumb_menu
+    breadcrumb(:farms)
+  end
+end
