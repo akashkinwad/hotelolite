db3fa16c870ed5901117c6d067f5fffb08754abc
Akash Kinwad <akashkinwad@gmail.com>
2020-03-29 12:10:27 +0530

    deploy

--- config/deploy/aws_e2e.rb
+++ config/deploy/aws_e2e.rb
@@ -1,4 +1,4 @@
 set :stage, :demo
-server '205.147.102.66', user: 'root', roles: %w{web app db}
+server '13.232.71.239', user: 'root', roles: %w{web app db}
 
 set :deploy_to, '/var/www/apps/hotelolite'
